189c58d8d8d4b131e4aef406c136263d7769a998
andreysuslov167-max <andreysuslov167@gmail.com>
2026-04-04 16:51:57 +0530

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
+
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    println!("welcome to ReeOS{}", "!");
+    panic!("panic OS{}", "!");
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    
     loop {}
 }
